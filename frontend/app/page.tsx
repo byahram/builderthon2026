3220d497f61911530c7b156265448a70f0824f3a
Ahram Kim <ahram0223@gmail.com>
2026-01-16 01:02:56 +0900

    feat: 타임스탬프 정확도 관련 안내 팝업 추가

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,12 @@
 
 import { MainLayout } from "@/components/layout/MainLayout";
+import { DisclaimerPopup } from "@/components/ui/DisclaimerPopup";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black text-white flex flex-col">
       <MainLayout />
+      <DisclaimerPopup />
     </main>
   );
 }
